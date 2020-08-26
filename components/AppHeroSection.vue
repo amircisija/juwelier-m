73ead9fb31dd03fa9249e8df98227f63f5c44cb8
Amir <amircisija@gmail.com>
2020-08-26 13:10:08 +0200

    setting up sections

---
+++ components/AppHeroSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="hero__section">
+    <b-jumbotron>
+      <b-container>
+        <b-row>
+          <b-col cols="6">
+            <h1>Juwerlier Melis</h1>
+            <p>For more information visit website</p>
+            <b-button variant="primary" href="#">More Info</b-button>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-jumbotron>
+  </section>
+</template>
+<style lang="scss">
+.jumbotron {
+  height: 60vh;
+}
+</style>
